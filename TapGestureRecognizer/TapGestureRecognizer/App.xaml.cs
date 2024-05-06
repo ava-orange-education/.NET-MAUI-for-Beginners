1bb9fb661c1739fa597de7a8de974f01427f4aae
Paul Johnson <paul@all-the-johnsons.co.uk>
2024-05-06 21:39:04 +0100

    Tap Gesture Recognizer

---
+++ TapGestureRecognizer/TapGestureRecognizer/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TapGestureRecognizer;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ TapGestureRecognizer/TapGestureRecognizer/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Input;
+
+namespace TapGestureRecognizer;
+
+public partial class MainPage : ContentPage
+{
+    bool _selected = false;
+    bool _otherSelected = false;
+    public ICommand LabelTappedCommand { get; set; }
+
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+        LabelTappedCommand = new Command(() =>
+        {
+            uiLabel2.Text = !_otherSelected ? "Wax on" : "Wax off";
+            _otherSelected = !_otherSelected;
+        });
+    }
+
+    void LabelTapped(object sender, TappedEventArgs e)
+    {
+        uiLabel.Text = !_selected ? "Hello" : "Goodbye";
+        _selected = !_selected;
+    }
+
+    void OnTapped()
+    {
+        uiLabel2.Text = !_otherSelected ? "Wax on" : "Wax off";
+        _otherSelected = !_otherSelected;
+    }
+}
