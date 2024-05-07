deefc6af08ac4648591d9b1fe2732c2d3335b3ee
Paul Johnson <paul@all-the-johnsons.co.uk>
2024-05-08 00:01:51 +0100

    Using images

---
+++ Chapter2/LoadingImages/LoadingImages/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace LoadingImages;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    
+    async void TapGestureRecognizer_OnTapped(object? sender, TappedEventArgs e)
+    {
+        try
+        {
+            var data = "";
+            using Stream fileStream = await FileSystem.Current.OpenAppPackageFileAsync("base64_1.txt");
+            using StreamReader reader = new StreamReader(fileStream);
+            data = reader.ReadToEnd();
+            byte[] image = Convert.FromBase64String(data);
+            MemoryStream ms = new (image);
+            sourceImage.Source = ImageSource.FromStream(() => ms);
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine(exception);
+            throw;
+        }
+    }
+}
