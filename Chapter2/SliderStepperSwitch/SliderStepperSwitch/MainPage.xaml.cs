5b6b767e31bb3d773fb27c4a2c5eb3da9c6cfef6
Paul Johnson <paul@all-the-johnsons.co.uk>
2024-05-10 02:05:25 +0100

    Slider, stepper and switch

---
+++ Chapter2/SliderStepperSwitch/SliderStepperSwitch/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+
+namespace SliderStepperSwitch;
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
+{
+    double slideValue;
+    int stepValue;
+
+    public bool IsToggleOn { get; set; } = false;
+
+    public double SlideValue
+    {
+        get => slideValue;
+        set { slideValue = value; OnPropertyChanged(); }
+    }
+
+    public int StepValue
+    {
+        get => stepValue;
+        set { stepValue = value; OnPropertyChanged(); }
+    }
+
+    public MainPage()
+    {
+        InitializeComponent();
+        SlideValue = 0;
+        StepValue = 0;
+        BindingContext = this;
+    }
+
+    void Slider_OnDragCompleted(object? sender, EventArgs e)
+    {
+        SlideValue = ((Slider)sender).Value;
+    }
+
+    void Stepper_OnValueChanged(object? sender, ValueChangedEventArgs e)
+    {
+        StepValue = Convert.ToInt32(e.NewValue);
+    }
+
+    void Switch_OnToggled(object? sender, ToggledEventArgs e)
+    {
+        IsToggleOn = e.Value;
+        uiLabel.Text = IsToggleOn ? "On" : "Off";
+    }
+}
