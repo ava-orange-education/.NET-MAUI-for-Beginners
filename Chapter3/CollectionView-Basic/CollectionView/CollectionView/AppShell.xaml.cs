9f9e337d6da079f0a16de156fc5a73a55f44e59e
Paul Johnson <paul@all-the-johnsons.co.uk>
2024-05-12 22:18:29 +0100

    Delete Chapter3 directory

--- Chapter3/CollectionView-Basic/CollectionView/CollectionView/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CollectionView;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- Chapter3/CollectionView-Basic/CollectionView/CollectionView/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace CollectionView;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
