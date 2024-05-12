c6c5c9c7b82adc06c0f7c6ad4d13fdd65793fea0
Paul Johnson <paul@all-the-johnsons.co.uk>
2024-05-12 22:27:15 +0100

    Observable Collection extension

---
+++ Chapter3/CollectionView-Basic/CollectionView/CollectionView/Helpers/ToObservableCollection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CollectionView.Helpers
+{
+    public static class ToObservableCollection
+    {
+        public static ObservableCollection<T> ToObservable<T>(this List<T> collection) => 
+            new ObservableCollection<T>(collection as IEnumerable<T>);
+    }
+}
