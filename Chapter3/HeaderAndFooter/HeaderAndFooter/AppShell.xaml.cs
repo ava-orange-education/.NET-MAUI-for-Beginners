66b3aed21774527bfbc8e7c4dae71f700dfe2e99
Paul Johnson <paul@all-the-johnsons.co.uk>
2024-05-13 21:28:21 +0100

    Header and Footer

---
+++ Chapter3/HeaderAndFooter/HeaderAndFooter/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace HeaderAndFooter;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Chapter3/HeaderAndFooter/HeaderAndFooter/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using HeaderAndFooter.Helpers;
+using HeaderAndFooter.Models;
+
+namespace HeaderAndFooter;
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
+{
+    ObservableCollection<CollectionModel>? routeInformation;
+    
+    public ObservableCollection<CollectionModel>? RouteInformation
+    {
+        get => routeInformation;
+        set
+        {
+            if (value?.Count != 0)
+            {
+                routeInformation = value;
+                OnPropertyChanged("Route");
+            }
+        }
+    }
+    
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        // get the data from a different class=
+        var info = new GenerateCollectionData().CreateBackingData();
+
+        RouteInformation = info.OrderBy(t => t.BusOperatorName).ToList().ToObservable();
+        
+        // set the binding context
+        BindingContext = this;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        // event registered to intercept when a value has changed
+        //PropertyChanged += PropertyHasChanged;
+    }
+
+    void PropertyHasChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        switch (e.PropertyName)
+        {
+            case "Route":
+                if (routeview.ItemsSource != null)
+                {
+                    routeview.ItemsSource = new ObservableCollection<CollectionModel>();
+                    MainThread.BeginInvokeOnMainThread(() => routeview.ItemsSource = RouteInformation);
+                }
+
+                break;
+        }
+    }
+}
