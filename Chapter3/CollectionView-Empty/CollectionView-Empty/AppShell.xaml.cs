897957f5c819f4446f2d86e535710fc084ade743
Paul Johnson <paul@all-the-johnsons.co.uk>
2024-05-18 09:43:53 +0100

    Empty collection view

---
+++ Chapter3/CollectionView-Empty/CollectionView-Empty/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CollectionView_Empty;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Chapter3/CollectionView-Empty/CollectionView-Empty/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace CollectionView_Empty;
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
+{
+
+    ObservableCollection<string> emptyCollection;
+    public ObservableCollection<string> EmptyCollection
+    {
+        get => emptyCollection;
+        set
+        {
+            emptyCollection = value;
+            OnPropertyChanged("Collection");
+        }
+    }
+    
+    public MainPage()
+    {
+        InitializeComponent();
+        EmptyCollection = new ObservableCollection<string>(); // empty collection
+        BindingContext = this;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        
+        // event registered to intercept when a value has changed
+        PropertyChanged += PropertyHasChanged;
+    }
+
+    void PropertyHasChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        switch (e.PropertyName)
+        {
+            case "Collection":
+                if (empty.ItemsSource != null)
+                {
+                    empty.ItemsSource = new ObservableCollection<string>();
+                    MainThread.BeginInvokeOnMainThread(() => empty.ItemsSource = EmptyCollection);
+                }
+                break;
+        }
+    }
+    
+    void Button_OnClicked(object? sender, EventArgs e)
+    {
+        EmptyCollection.Add("");
+    }
+}
