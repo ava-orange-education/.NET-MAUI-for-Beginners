2c8798ba26717d34c26ccc5c9c5bd845f90c99d9
Paul Johnson <paul@all-the-johnsons.co.uk>
2024-05-19 11:44:07 +0100

    Simple absolutelayout demo

---
+++ Chapter3/AbsoluteLayout/AbsLayout/AbsLayout/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AbsLayout;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ Chapter3/AbsoluteLayout/AbsLayout/AbsLayout/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AbsLayout;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
