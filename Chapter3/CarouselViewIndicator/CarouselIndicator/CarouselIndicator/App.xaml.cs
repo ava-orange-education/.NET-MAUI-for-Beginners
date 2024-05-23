cc9b2938788b448ba86ba0ce09e9f06a1f6d7866
Paul Johnson <paul@all-the-johnsons.co.uk>
2024-05-23 22:47:48 +0100

    CarouselfView with Indicator

---
+++ Chapter3/CarouselViewIndicator/CarouselIndicator/CarouselIndicator/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CarouselIndicator;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ Chapter3/CarouselViewIndicator/CarouselIndicator/CarouselIndicator/Models/Transport.cs
@@ -0,0 +1,7 @@
+﻿namespace CarouselIndicator.Models;
+
+public class Transport
+{
+    public string OperatorName { get; set; }
+    public string Filename { get; set; }
+}
