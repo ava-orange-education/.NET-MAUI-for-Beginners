02dd7716d38859c644aa9b5855ae4a48adfb30e0
Paul Johnson <paul@all-the-johnsons.co.uk>
2024-06-22 13:14:05 +0100

    Add files via upload

---
+++ Chapter15/AnimatedGifs/AnimatedGif/AnimatedGif/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimatedGif;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Chapter15/AnimatedGifs/AnimatedGif/AnimatedGif/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimatedGif;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
